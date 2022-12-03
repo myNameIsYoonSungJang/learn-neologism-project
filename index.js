567916f9a664f72f2a63fa1d62c94f0f90ba4a34
YoonSungJang <jang064880@gmail.com>
2022-12-03 23:22:01 +0900

    2022-12-03 index.html completion

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function () {
+  console.log('listening on 8080')
+})
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/templates/index.html')
+})
+
+app.get('/introduce', function(req, res){
+  res.sendfile(__dirname + '/templates/introduce.html')
+})
+
+app.get('/survey', function(req, res){
+  res.sendfile(__dirname + '/templates/survey.html')
+})
+
+app.get('/dictionary', function(req, res){
+  res.sendfile(__dirname + '/templates/dictionary.html')
+})
+
+app.get('/quiz', function(req, res){
+  res.sendfile(__dirname + '/templates/quiz.html')
+})
